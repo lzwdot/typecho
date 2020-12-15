d1a315b4e548562e24b614fa40b32d1fbc061114
阿伟 <bob.922@qq.com>
2020-12-15 18:18:08 +0800

    css

--- themes/gravatar/sidebar.php
+++ themes/gravatar/sidebar.php
@@ -8,68 +8,86 @@
         </form>
     </section>
 
-    <?php if (!empty($this->options->sidebarBlock) && in_array('ShowRecentPosts', $this->options->sidebarBlock)): ?>
-        <section class="widget">
-            <h3 class="widget-title"><?php _e('最新文章'); ?></h3>
-            <ul class="widget-list">
-                <?php $this->widget('Widget_Contents_Post_Recent')
-                    ->parse('<li><a href="{permalink}">{title}</a></li>'); ?>
-            </ul>
-        </section>
-    <?php endif; ?>
+    <?php if($this->is('post')): ?>
 
-    <?php if (!empty($this->options->sidebarBlock) && in_array('ShowRecentComments', $this->options->sidebarBlock)): ?>
-        <section class="widget">
-            <h3 class="widget-title"><?php _e('最近回复'); ?></h3>
+        <section class="widget article-catalog">
+            <h3 class="widget-title"><?php _e('文章导航'); ?></h3>
             <ul class="widget-list">
-                <?php $this->widget('Widget_Comments_Recent')->to($comments); ?>
-                <?php while($comments->next()): ?>
-                    <li><a href="<?php $comments->permalink(); ?>"><?php $comments->author(false); ?></a>: <?php $comments->excerpt(35, '...'); ?></li>
-                <?php endwhile; ?>
-            </ul>
-        </section>
-    <?php endif; ?>
-
-    <?php if (!empty($this->options->sidebarBlock) && in_array('ShowCategory', $this->options->sidebarBlock)): ?>
-        <section class="widget">
-            <h3 class="widget-title"><?php _e('分类'); ?></h3>
-            <?php $this->widget('Widget_Metas_Category_List')->listCategories('wrapClass=widget-list'); ?>
-        </section>
-    <?php endif; ?>
-
-    <?php if (!empty($this->options->sidebarBlock) && in_array('ShowArchive', $this->options->sidebarBlock)): ?>
-        <section class="widget">
-            <h3 class="widget-title"><?php _e('归档'); ?></h3>
-            <ul class="widget-list">
-                <?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=F Y')
-                    ->parse('<li><a href="{permalink}">{date}</a></li>'); ?>
-            </ul>
-        </section>
-    <?php endif; ?>
-
-    <?php if (!empty($this->options->sidebarBlock) && in_array('ShowOther', $this->options->sidebarBlock)): ?>
-        <section class="widget">
-            <h3 class="widget-title"><?php _e('其它'); ?></h3>
-            <ul class="widget-list">
-                <?php if($this->user->hasLogin()): ?>
-                    <li class="last"><a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a></li>
-                    <li><a href="<?php $this->options->logoutUrl(); ?>"><?php _e('退出'); ?></a></li>
-                <?php else: ?>
-                    <li class="last"><a href="<?php $this->options->adminUrl('login.php'); ?>"><?php _e('登录'); ?></a></li>
+                <?php if(isset($this->categories[0])): ?>
+                    <?php $this->widget('Widget_Archive@myCustomCategory', 'type=category&pageSize=50', 'mid='.$this->categories[0]['mid'])->to($categoryPosts); $categoryPosts = array_reverse($categoryPosts->stack)?>
+                    <?php while($categoryPost = current($categoryPosts)): next($categoryPosts); ?>
+                        <li class="<?php if($this->cid === $categoryPost['cid']): ?> current <?php endif; ?>"><a itemprop="url" href="<?php echo $categoryPost['permalink'] ?>"><?php echo $categoryPost['title'] ?></a></li>
+                    <?php endwhile; ?>
                 <?php endif; ?>
-                <li><a href="<?php $this->options->feedUrl(); ?>"><?php _e('文章 RSS'); ?></a></li>
-                <li><a href="<?php $this->options->commentsFeedUrl(); ?>"><?php _e('评论 RSS'); ?></a></li>
             </ul>
         </section>
-    <?php endif; ?>
 
-    <?php if ($this->options->links): ?>
-        <section class="widget">
-            <h3 class="widget-title"><?php _e('链接'); ?></h3>
-            <div>
-                <?php  _e($this->options->links()); ?>
-            </div>
-        </section>
+    <?php else: ?>
+
+        <?php if (!empty($this->options->sidebarBlock) && in_array('ShowRecentPosts', $this->options->sidebarBlock)): ?>
+            <section class="widget">
+                <h3 class="widget-title"><?php _e('最新文章'); ?></h3>
+                <ul class="widget-list">
+                    <?php $this->widget('Widget_Contents_Post_Recent')
+                        ->parse('<li><a href="{permalink}">{title}</a></li>'); ?>
+                </ul>
+            </section>
+        <?php endif; ?>
+
+        <?php if (!empty($this->options->sidebarBlock) && in_array('ShowRecentComments', $this->options->sidebarBlock)): ?>
+            <section class="widget">
+                <h3 class="widget-title"><?php _e('最近回复'); ?></h3>
+                <ul class="widget-list">
+                    <?php $this->widget('Widget_Comments_Recent')->to($comments); ?>
+                    <?php while($comments->next()): ?>
+                        <li><a href="<?php $comments->permalink(); ?>"><?php $comments->author(false); ?></a>: <?php $comments->excerpt(35, '...'); ?></li>
+                    <?php endwhile; ?>
+                </ul>
+            </section>
+        <?php endif; ?>
+
+        <?php if (!empty($this->options->sidebarBlock) && in_array('ShowCategory', $this->options->sidebarBlock)): ?>
+            <section class="widget">
+                <h3 class="widget-title"><?php _e('分类'); ?></h3>
+                <?php $this->widget('Widget_Metas_Category_List')->listCategories('wrapClass=widget-list'); ?>
+            </section>
+        <?php endif; ?>
+
+        <?php if (!empty($this->options->sidebarBlock) && in_array('ShowArchive', $this->options->sidebarBlock)): ?>
+            <section class="widget">
+                <h3 class="widget-title"><?php _e('归档'); ?></h3>
+                <ul class="widget-list">
+                    <?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=F Y')
+                        ->parse('<li><a href="{permalink}">{date}</a></li>'); ?>
+                </ul>
+            </section>
+        <?php endif; ?>
+
+        <?php if (!empty($this->options->sidebarBlock) && in_array('ShowOther', $this->options->sidebarBlock)): ?>
+            <section class="widget">
+                <h3 class="widget-title"><?php _e('其它'); ?></h3>
+                <ul class="widget-list">
+                    <?php if($this->user->hasLogin()): ?>
+                        <li class="last"><a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a></li>
+                        <li><a href="<?php $this->options->logoutUrl(); ?>"><?php _e('退出'); ?></a></li>
+                    <?php else: ?>
+    <!--                    <li class="last"><a href="--><?php //$this->options->adminUrl('login.php'); ?><!--">--><?php //_e('登录'); ?><!--</a></li>-->
+                    <?php endif; ?>
+                    <li><a href="<?php $this->options->feedUrl(); ?>"><?php _e('文章 RSS'); ?></a></li>
+    <!--                <li><a href="--><?php //$this->options->commentsFeedUrl(); ?><!--">--><?php //_e('评论 RSS'); ?><!--</a></li>-->
+                </ul>
+            </section>
+        <?php endif; ?>
+
+        <?php if ($this->options->links): ?>
+            <section class="widget">
+                <h3 class="widget-title"><?php _e('链接'); ?></h3>
+                <div>
+                    <?php  _e($this->options->links()); ?>
+                </div>
+            </section>
+        <?php endif; ?>
+
     <?php endif; ?>
 
 </div><!-- end #sidebar -->
